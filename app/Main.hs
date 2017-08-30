bc7d79677ecb724835bc9199eb70c9de63a403ac
Michael Maier <maier1michael@gmail.com>
2017-08-30 10:16:41 +0200

    render SVG (not working correct yet)

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Codec.Picture
@@ -16,14 +18,55 @@ import Data.Vector.Storable (Vector)
 import qualified Data.Vector.Storable as V
 
 import Foreign.C.Types (CInt)
-import SDL.Vect (V2(V2))
 import qualified SDL
+import SDL.Vect (Point(P), V2(V2), V4(V4))
+
+import Control.Concurrent (threadDelay)
+import SDL (($=))
 
 import Paths_svg_loading (getDataFileName)
+
+screenWidth, screenHeight :: CInt
+(screenWidth, screenHeight) = (1280, 720)
 
 -- SDL dependency: sudo apt-get install libsdl2-dev
 main :: IO ()
 main = do
+  SDL.initialize [SDL.InitVideo, SDL.InitTimer, SDL.InitEvents]
+    -- ensure render quality
+  SDL.HintRenderScaleQuality $= SDL.ScaleLinear
+  do renderQuality <- SDL.get SDL.HintRenderScaleQuality
+     CM.when (renderQuality /= SDL.ScaleLinear) $
+       putStrLn "Warning: Linear texture filtering not enabled!"
+  window <-
+    SDL.createWindow
+      "Load and render SVG"
+      SDL.defaultWindow
+      { SDL.windowPosition = SDL.Centered
+      , SDL.windowInitialSize = V2 screenWidth screenHeight
+      }
+  SDL.showWindow window
+  renderer <-
+    SDL.createRenderer
+      window
+      (-1)
+      SDL.RendererConfig
+      { SDL.rendererType = SDL.AcceleratedVSyncRenderer
+      , SDL.rendererTargetTexture = True
+      }
+  SDL.rendererDrawColor renderer $= V4 110 132 174 maxBound
+  SDL.clear renderer
+  renderSvgExample renderer
+  SDL.present renderer
+  threadDelay 2000000
+  SDL.destroyRenderer renderer
+  SDL.destroyWindow window
+  SDL.quit
+
+rect x y w h = SDL.Rectangle (P $ V2 x y) (V2 w h)
+
+renderSvgExample :: SDL.Renderer -> IO ()
+renderSvgExample renderer = do
   mimage <- getDataFileName "thumbs-up.svg" >>= loadSVGImage
   case mimage of
     Nothing -> putStrLn "Image convertion failed."
@@ -31,8 +74,23 @@ main = do
       let surfaceSize :: V2 CInt
           surfaceSize = V2 1280 720
       surface <- createSurfaceFromSVG image surfaceSize
-            -- show the surface
-      putStrLn "Hopefully showing the image."
+      texture <- SDL.createTextureFromSurface renderer surface
+      V2 surfaceWidth surfaceHeight <- SDL.surfaceDimensions surface
+      SDL.freeSurface surface
+      let source = rect 0 0 surfaceWidth surfaceHeight
+          dest = rect 0 0 surfaceWidth surfaceHeight
+          angle = 0.0
+          center = Nothing
+          flipNone = V2 False False
+      SDL.copyEx
+        renderer
+        texture
+        (Just source)
+        (Just dest)
+        angle
+        center
+        flipNone
+      SDL.destroyTexture texture
 
 createSurfaceFromSVG :: Image PixelRGBA8 -> V2 CInt -> IO SDL.Surface
 createSurfaceFromSVG image surfaceSize = do
@@ -56,7 +114,7 @@ convertToMutableVector v = do
 
 loadSVGImage :: FilePath -> IO (Maybe (Image PixelRGBA8))
 loadSVGImage filepath = do
-  mdoc <-  loadSvgFile filepath
+  mdoc <- loadSvgFile filepath
   case mdoc of
     Nothing -> return Nothing
     Just doc -> do
